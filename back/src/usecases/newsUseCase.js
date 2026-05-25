a614e2f61923c8c8203e81c4c656d79b4bd571f6
kum4san5 <kum4rt1san5@gmail.com>
2026-05-26 00:56:53 +0900

    Delete back directory
    a

--- back/src/usecases/newsUseCase.js
+++
@@ -1,8 +0,0 @@
-var Autonote = Autonote || {};
-Autonote.Usecases = Autonote.Usecases || {};
-
-Autonote.Usecases.NewsUseCase = {
-  fetchArticles(siteUrl) {
-    return Autonote.Infrastructure.FeedParser.fetchNewsContent(siteUrl);
-  }
-};
