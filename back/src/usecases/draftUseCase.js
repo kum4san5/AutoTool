3130aa2b2934717a242605f878d633c99feae0d3
Kumazawa <kumazawakousuke@gmail.com>
2026-05-26 01:15:30 +0900

    管理修正

--- back/src/usecases/draftUseCase.js
+++
@@ -1,8 +0,0 @@
-var Autonote = Autonote || {};
-Autonote.Usecases = Autonote.Usecases || {};
-
-Autonote.Usecases.DraftUseCase = {
-  createArticleRecord(article, generated, publishResult) {
-    return Autonote.Infrastructure.Storage.createArticleRecord(article, generated, publishResult);
-  }
-};
--- back/src/utils/index.js
+++
@@ -1,2 +0,0 @@
-var Autonote = Autonote || {};
-Autonote.Utils = Autonote.Utils || {};
